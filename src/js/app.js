ce9954750c3b3f5d63653fa8f358e6208574c531
Mason Fox <masonfox22@gmail.com>
2018-06-12 07:02:50 -0400

    menu mobile design and desktop header fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 // import 'bootstrap'
 
 // Lame jQuery event driven stuff
-$(() => {
+$(function () {
   
   // shrink nav after landing watcher
-  $(window).on('scroll', (e) => {
+  $(window).on('scroll', function (e) {
     let landingHeight = $('.landing').height()
     let currentHeight = $(document).scrollTop()
     
@@ -13,5 +13,15 @@ $(() => {
     } else {
       $('.main-nav').removeClass('shrink')
     }
+  })
+  
+  $('.menu-toggle').on('click', function () {
+    $('.menu-toggle').toggleClass('active')
+    $('.main-nav .nav-right').toggleClass('active')
+  })
+  
+  $('.main-nav .nav-right a').on('click', function () {
+    $('.menu-toggle').removeClass('active')
+    $('.main-nav .nav-right').removeClass('active')
   })
 })
